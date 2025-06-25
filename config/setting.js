d6175b8fdb73f2d4b0f4444b00ab373aea48ceac
Ahlul <ahlul963@gmail.com>
2025-06-26 03:02:01 +0700

    Remove unused libraries and functions from the project, including mediafire downloader, myfunc utilities, scraper functions, spam filters, uploader utilities, and EXIF data file. This cleanup enhances code maintainability and reduces unnecessary dependencies.

--- config/setting.js
+++ config/setting.js
@@ -31,9 +31,6 @@ global.keyopenai = "ISI_APIKEY_OPENAI_DISINI";
 global.keyopenai = "2d2703d1";
 global.nomorowner = "https://wa.me/628126915328";
 
-// Scrap Lib
-global.scrap = new (require("../function/lib/lib.scrap.js"))();
-
 global.region = "I`m From Indonesia";
 global.prefa = ["", "!", ".", "#", "-", "•"];
 global.thumb = fs.readFileSync("./function/image/thumb.jpg");
